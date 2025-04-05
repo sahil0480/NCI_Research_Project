999b3bd082c9edddd86cffec5f2667ebd299b704
Sahil <sahil_048@Sahils-MacBook-Pro.local>
2025-04-05 20:12:48 +0100

    Final Static Issue

--- rest_mgmt/wsgi.py
+++ rest_mgmt/wsgi.py
@@ -1,21 +1,12 @@
-"""
-WSGI config for rest_mgmt project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_mgmt.settings')
 
 application = get_wsgi_application()
 
-# ✅ This should point to /app/staticfiles, not rest_mgmt/staticfiles
+# Serve static files with WhiteNoise
 BASE_DIR = Path(__file__).resolve().parent.parent
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
