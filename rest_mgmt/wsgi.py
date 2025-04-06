0a3c947058e084006656c30c8ed96b94bdffbc8e
Sahil <sahil_048@Sahils-MacBook-Pro.local>
2025-04-06 13:49:38 +0100

    Static Error

--- rest_mgmt/wsgi.py
+++ rest_mgmt/wsgi.py
@@ -1,12 +1,12 @@
 import os
-from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_mgmt.settings')
 
 application = get_wsgi_application()
 
-# Serve static files with WhiteNoise
+# Correct root path for collected static files
 BASE_DIR = Path(__file__).resolve().parent.parent
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
